57770c33cc75e944ce17fc4e68512fe5c4f7c598
ankit <ankitiwari26@gmail.com>
2024-02-13 16:13:55 +0530

    Initial commit

---
+++ jwtExample3/jwtExample3/src/main/java/com/jwt/example/entity/User.java
@@ -0,0 +1,100 @@
+package com.jwt.example.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+
+
+@Entity
+@Table(name="user_table")
+public class User  implements UserDetails {
+ public User() {
+		super();
+
+	}
+
+public User(String userId, String name, String email, String password) {
+	super();
+	this.userId = userId;
+	this.name = name;
+	this.email = email;
+	this.password = password;
+}
+
+@Id
+    private String userId;
+    private String name;
+    private String email;
+
+    public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
+
---
+++ jwtExample3/jwtExample3/src/main/java/com/jwt/example/model/JwtRequest.java
@@ -0,0 +1,36 @@
+package com.jwt.example.model;
+
+import lombok.*;
+
+
+@Builder
+public class JwtRequest {
+
+    @Override
+	public String toString() {
+		return "JwtRequest [email=" + email + ", password=" + password + "]";
+	}
+	private String email;
+    private  String password;
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public JwtRequest(String email, String password) {
+		super();
+		this.email = email;
+		this.password = password;
+	}
+	public JwtRequest() {
+		super();
+	}
+    
+}
